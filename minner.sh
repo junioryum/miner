6be83db0ad9d467e578c1d9bbcebf9dbba054003
junioryum <31814429+junioryum@users.noreply.github.com>
2018-01-07 14:37:23 -0200

    Update minner.sh

--- minner.sh
+++ minner.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 apt-get update -y
- 
+  apt-get install automake autoconf pkg-config libcurl4-openssl-dev libjansson-dev libssl-dev libgmp-dev make g++
 apt-get install  build-essential -y
 apt-get install libcurl4-openssl-dev g++ gcc -y
 
@@ -14,7 +14,7 @@ chmod +x autogen.sh
 ./configure CFLAGS="-O3" --with-crypto --with-curl 
 make && make install 
 cd cpuminer-multi 
-  screen ./minerd -a cryptonight -o stratum+tcp://xmr.pool.minergate.com:45560 -u junior_yum@hotmail.com -p x
+  screen ./cpuminer -a scrypt -o stratum+tcp://scrypt.eobot.com:4444 --userpass eobot.1149745:x
  
 echo Repositorio Atualizado.
 echo Sistema Atualizado.
