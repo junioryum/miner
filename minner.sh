15faae56944a929bdf387a08c35a4401855078e5
junioryum <31814429+junioryum@users.noreply.github.com>
2018-01-07 11:54:25 -0200

    Update minner.sh

--- minner.sh
+++ minner.sh
@@ -2,7 +2,7 @@
 apt-get update -y
  
 apt-get install  build-essential -y
-apt-get install libcurl4-openssl-dev git -y
+apt-get install libcurl4-openssl-dev g++ gcc -y
 
  apt-get install autotools-dev autoconf -y
  apt-get install libcurl3 libcurl4-gnutls-dev -y
